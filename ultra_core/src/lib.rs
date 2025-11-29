e012490f7c5192146f3e577df128b7ef7c19f883
Alex <186313861+atbu@users.noreply.github.com>
2025-11-29 19:08:10 +0000

    Implement simple single-rotor, single-letter transformation

--- ultra_core/src/lib.rs
+++ ultra_core/src/lib.rs
@@ -1,5 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn map_through_rotor(signal: u8, wiring: [u8; 26], starting_position: u8) -> u8 {
+    let after_entrance_offset = (signal + starting_position) % 26;
+    let after_lookup = wiring[after_entrance_offset as usize];
+    let after_exit_offset = (after_lookup - starting_position) % 26;
+
+    after_exit_offset
+}
+
+/// Converts a character to an integer index, where 'A' equals 0, 'B' equals 1, ..., 'Z' equals 25.
+pub fn char_to_index(char: char) -> u8 {
+    char as u8 - 65
 }
 
 #[cfg(test)]
@@ -7,8 +16,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_char_to_index() {
+        assert_eq!(char_to_index('A'), 0);
+        assert_eq!(char_to_index('B'), 1);
+        assert_eq!(char_to_index('C'), 2);
+        assert_eq!(char_to_index('Z'), 25);
+    }
+
+    #[test]
+    fn simple_transformation() {
+        let wiring_string: &str = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+        let mut wiring: [u8; 26] = [0; 26];
+        for (index, character) in wiring_string.chars().enumerate() {
+            wiring[index] = char_to_index(character);
+        }
+        assert_eq!(map_through_rotor(char_to_index('A'), wiring, 1), char_to_index('J'));
     }
 }
