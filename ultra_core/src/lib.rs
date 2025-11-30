0e2bdcc49ef54b4ef747f08efd90d4cf1a5d5dee
Alex <186313861+atbu@users.noreply.github.com>
2025-11-30 10:15:46 +0000

    Add documentation to `char_to_index` and `index_to_char`

--- ultra_core/src/lib.rs
+++ ultra_core/src/lib.rs
@@ -1,7 +1,11 @@
+/// Converts a character to a zero-based index related to its position in the alphabet, where 'A'
+/// equals 0, 'B' equals 1, ..., 'Z' equals 25.
 pub fn char_to_index(char: char) -> u8 {
     char as u8 - 65
 }
 
+/// Converts a zero-based index into a character, where 0 equals 'A', 1 equals 'B', ..., 25 equals
+/// 'Z'.
 pub fn index_to_char(index: u8) -> char {
     (index + 65) as char
 }
