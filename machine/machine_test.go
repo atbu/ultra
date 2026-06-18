9aa28282eac210d19ff205283956dabee32a2472
Alex Burrows <186313861+atbu@users.noreply.github.com>
2026-06-18 20:42:27 +0100

    Add helper functions to machine_test, add reciprocity test and update sanity check test to new structure

--- machine/machine_test.go
+++ machine/machine_test.go
@@ -3,9 +3,23 @@ package machine
 import (
 	"testing"
 
+	"github.com/atbu/ultra/conversion"
 	"github.com/atbu/ultra/reflector"
 	"github.com/atbu/ultra/rotor"
 )
+
+func (m *Machine) updateRotorPositions(left rune, middle rune, right rune) {
+	m.LeftRotor.Position = conversion.ConvertCharToIndex(left)
+	m.MiddleRotor.Position = conversion.ConvertCharToIndex(middle)
+	m.RightRotor.Position = conversion.ConvertCharToIndex(right)
+}
+
+func runTest(t *testing.T, machine *Machine, input string, expectedOutput string) {
+	actualOutput := machine.Process(input)
+	if actualOutput != expectedOutput {
+		t.Errorf("machine.Process(%s) failed - got=%s, expected=%s", input, actualOutput, expectedOutput)
+	}
+}
 
 func TestSanityCheck(t *testing.T) {
 	machine := &Machine{
@@ -18,9 +32,27 @@ func TestSanityCheck(t *testing.T) {
 	}
 
 	input := "AAAAA"
-	actualOutput := machine.Process(input)
 	expectedOutput := "BDZGO"
-	if actualOutput != expectedOutput {
-		t.Errorf("machine.Process(%s) failed - got=%s, expected=%s", input, actualOutput, expectedOutput)
+	runTest(t, machine, input, expectedOutput)
+}
+
+func TestReciprocity(t *testing.T) {
+	machine := &Machine{
+		rotor.NewRotor(rotor.RotorI, 'M', 'A'),
+		rotor.NewRotor(rotor.RotorII, 'C', 'A'),
+		rotor.NewRotor(rotor.RotorIII, 'K', 'A'),
+		nil,
+		reflector.New(reflector.ReflectorB),
+		nil,
 	}
+
+	input := "ENIGMA"
+	expectedOutput := "QMJIDO"
+	runTest(t, machine, input, expectedOutput)
+
+	machine.updateRotorPositions('M', 'C', 'K')
+
+	input = "AAAAA"
+	expectedOutput = "YWDVQ"
+	runTest(t, machine, input, expectedOutput)
 }
