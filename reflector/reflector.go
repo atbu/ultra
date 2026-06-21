f161e90dedcd3fd6605203517638b730944778f5
Alex Burrows <186313861+atbu@users.noreply.github.com>
2026-06-21 21:58:31 +0100

    Add doc comments for reflector.go

--- reflector/reflector.go
+++ reflector/reflector.go
@@ -4,23 +4,28 @@ import "github.com/atbu/ultra/conversion"
 
 type ReflectorConfiguration int
 
+// The constants below represent the reflector configurations found in Enigma machines.
+// Reflectors A, B and C are found in all Enigma machines, whereas the narrow reflectors are found in Enigma M4, and
+// are used when the fourth rotor is installed.
 const (
 	ReflectorA ReflectorConfiguration = iota
 	ReflectorB
 	ReflectorC
-
-	// The two below are used in Enigma M4.
 	ReflectorNarrowB
 	ReflectorNarrowC
 )
 
+// The Reflector structure simply contains the wiring array which describes how each character is mapped to another
+// character.
 type Reflector struct {
 	Wiring [26]int
 }
 
+// The New function constructs a new Reflector from its configuration.
 func New(configuration ReflectorConfiguration) *Reflector {
 	var wiringString string
 
+	// The wiring strings of each reflector are defined here.
 	switch configuration {
 	case ReflectorA:
 		wiringString = "EJMZALYXVBWFCRQUONTSPIKHGD"
@@ -34,11 +39,14 @@ func New(configuration ReflectorConfiguration) *Reflector {
 		wiringString = "RDOBJNTKVEHMLFCWZAXGYIPSUQ"
 	}
 
+	// Convert the wiring string to an array and return the constructed Reflector.
 	return &Reflector{
 		conversion.ConvertWiringStringToArray(wiringString),
 	}
 }
 
+// The MapSignal function defines how we map a signal through a reflector - we simply look up the other character our
+// input signal refers to and output it.
 func (r *Reflector) MapSignal(signal int) int {
 	return r.Wiring[signal]
 }
